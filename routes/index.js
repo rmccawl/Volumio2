d2309e8780ad80efb2e5063e717e10c687954f78
volumio <inf@volumio.org>
2015-06-12 23:34:35 +0000

    Added About Page

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,13 @@ var express = require('express');
 var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Volumio Test Player' });
+  res.render('index', { title: 'Volumio - Audiophile Music Player' });
 });
 router.get('/debug', function(req, res) {
-  res.render('debug', { title: 'Volumio Test Player' });
+  res.render('debug', { title: 'Debug - Volumio' });
+});
+router.get('/about', function(req, res) {
+  res.render('about', { title: 'About - Volumio' });
 });
 module.exports = router;
 
